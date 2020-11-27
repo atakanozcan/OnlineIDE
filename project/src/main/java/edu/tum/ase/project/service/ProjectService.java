9f3b47e3397553fcdd074f15d6fa1ed004db6702
yusufsamsum <yusuf.samsum@ug.bilkent.edu.tr>
2020-11-27 15:41:20 +0100

    Adding first PGSQL connection module

---
+++ project/src/main/java/edu/tum/ase/project/service/ProjectService.java
@@ -0,0 +1,30 @@
+package edu.tum.ase.project.service;
+
+import edu.tum.ase.project.model.Project;
+import edu.tum.ase.project.repository.ProjectRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+@Service
+public class ProjectService {
+    @Autowired
+    private ProjectRepository projectRepository;
+    public Project createProject(Project project) {
+        try {
+            return projectRepository.save(project);
+        }
+        catch (IllegalArgumentException e)
+        {
+            e.printStackTrace();
+            return null;
+        }
+    }
+    public Project findByName(String name) {
+        return projectRepository.findByName(name);
+    }
+    public List<Project> getProjects() {
+        List<Project> list = projectRepository.findAll();
+        return list;
+    }
+}
