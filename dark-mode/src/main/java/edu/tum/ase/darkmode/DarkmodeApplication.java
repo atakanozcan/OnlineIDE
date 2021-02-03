d03422d72dd57dd268d0b32fcd5188ff13ba4e1b
Atakan Ozcan <ge79vef@tum.de>
2021-02-03 03:24:22 +0300

    Exercise 4: Service Discovery in the OnlineIDE sheet 12 rebase master

--- dark-mode/src/main/java/edu/tum/ase/darkmode/DarkmodeApplication.java
+++ dark-mode/src/main/java/edu/tum/ase/darkmode/DarkmodeApplication.java
@@ -1,12 +1,14 @@
 package edu.tum.ase.darkmode;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
+@EnableEurekaClient
 public class DarkmodeApplication {
 	private boolean isDarkModeOn = false;
 
--- project/src/main/java/edu/tum/ase/project/ProjectApplication.java
+++ project/src/main/java/edu/tum/ase/project/ProjectApplication.java
@@ -12,6 +12,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableOAuth2Client;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
 
 import javax.sql.DataSource;
 import java.util.List;
@@ -20,6 +22,7 @@ import java.util.List;
 @EnableResourceServer
 @EnableOAuth2Client
 @SpringBootApplication
+@EnableEurekaClient
 public class ProjectApplication implements CommandLineRunner {
 	private static final Logger log = LoggerFactory.getLogger(ProjectApplication.class);
 
@@ -28,7 +31,7 @@ public class ProjectApplication implements CommandLineRunner {
 
 	@Autowired
 	ProjectService projectService;
-	
+
 	@Autowired
 	SourceFileService sourceFileService;
 
@@ -39,26 +42,26 @@ public class ProjectApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		log.info("DataSource = " + dataSource);
-		
+
 		String projectName = "my-project";
-		
+
 		while(projectService.findByName(projectName) != null) {
 			log.info("The project name " + projectName + " is already taken.");
 			projectName = projectName + Math.round(Math.random()*10);
 		}
-		
-		Project project = projectService.createProject(new Project(projectName)); 
+
+		Project project = projectService.createProject(new Project(projectName));
 		log.info("ID of saved project = " + project.getId());
-		
+
 		Project p = projectService.findByName(projectName);
 		log.info("ID of queried project = " + p.getId());
-		
+
 		List<Project> projects = projectService.getProjects();
 		log.info("Length of project list = " + projects.size());
 
 		SourceFile sourceFile = sourceFileService.createSourceFile(new SourceFile(project, "testfile"));
 		log.info("Source file saved, name: " + sourceFile.getName() + " project ID: " + sourceFile.getProject().getId());
-		
+
 		SourceFile s = sourceFileService.findByProjectAndName(project, "testfile");
 		log.info("Source was queried, name: " + s.getName() + " project ID: " + s.getProject().getId());
 	}
